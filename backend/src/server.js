336737153b702b54e44f685705f0f80368151997
Tushar-Shinde45 <149402930+Tushar-Shinde45@users.noreply.github.com>
2025-07-16 13:53:59 +0530

    fix: remove extra whitespace in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js"
 
-const app = express();
+const app = express(); 
 
 app.use(cors());
 app.use(express.json());
